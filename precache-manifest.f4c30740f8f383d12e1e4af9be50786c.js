1e3a889a9cdef222cf348eec1cfe7e362248d8d0
Analista 5 <analista5@MERCADEOVIRTUAL.local>
2023-02-10 11:37:00 -0500

    Updates

---
+++ precache-manifest.f4c30740f8f383d12e1e4af9be50786c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0acc8982f4be8ca8b028",
+    "url": "/Pokedex/static/css/main.ee351278.chunk.css"
+  },
+  {
+    "revision": "0acc8982f4be8ca8b028",
+    "url": "/Pokedex/static/js/main.0acc8982.chunk.js"
+  },
+  {
+    "revision": "10d8c738504b8f84b6e0",
+    "url": "/Pokedex/static/js/1.10d8c738.chunk.js"
+  },
+  {
+    "revision": "0d08562635c88cc5a0e8",
+    "url": "/Pokedex/static/js/2.0d085626.chunk.js"
+  },
+  {
+    "revision": "3a5d62e89c478e9d6f80",
+    "url": "/Pokedex/static/js/runtime~main.3a5d62e8.js"
+  },
+  {
+    "revision": "1a7ddc8ddcaa6d1759c4630fc5bb7188",
+    "url": "/Pokedex/index.html"
+  }
+];
--- src/Views/noFound/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NoFound = () => (
-    <div>
-        <p>Pagina no encontrada.</p>
-    </div>
-)
-
-export default NoFound;
